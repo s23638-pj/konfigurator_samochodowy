eec2857347fcf6bd2fb18710d7c75f2083e739dd
Wiktor Krieger <s23638@pjwstk.edu.pl>
2021-10-16 10:03:03 +0200

    Initial commit

---
+++ src/main/java/com/example/konfigurator/KonfiguratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.konfigurator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonfiguratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonfiguratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/konfigurator/KonfiguratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.konfigurator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonfiguratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
